2357d64f5c6e8f9f4b72038bac11e83cbb838225
reyLipa <109700575+reyLipa@users.noreply.github.com>
2023-09-09 00:50:49 -0300

    Initial commit

---
+++ src/AccesoADatos/Conexion.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AccesoADatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Isabel
+ */
+public class Conexion {
+    private static final String URL="jdbc:mariadb://localhost/";
+    private static final String DB="grupo30ULP";
+    private static final String USER="root";
+    private static final String PASSWORD="";
+    private static Connection connection;
+
+    private Conexion() {
+        
+    }
+    public static Connection getConexion(){
+        if (connection==null) {
+            
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection=DriverManager.getConnection(URL+DB, USER, PASSWORD);
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "error al cargar el Driver");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "error de conexion a la base de datos");
+            }
+            
+        }
+        return connection;
+    }
+    
+    
+    
+}
---
+++ src/grupo30ulp/Grupo30ULP.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupo30ulp;
+
+import AccesoADatos.Conexion;
+import java.sql.Connection;
+
+/**
+ *
+ * @author LAu
+ */
+public class Grupo30ULP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Connection conex=Conexion.getConexion();
+    }
+    
+}
